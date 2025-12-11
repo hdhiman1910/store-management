f8543fc1d4854070a1dabe5a0caaacc7894782a9
Harsh Dhiman <hdhiman1910@gmail.com>
2025-12-11 23:59:53 +0530

    add registration endpoint and user creation logic

--- backend/resources/auth_resource.py
+++ backend/resources/auth_resource.py
@@ -1,8 +1,10 @@
 "/login"
+"/register"
 
 from flask import Blueprint, request, jsonify
-from flask_security import verify_password
-from models import User
+from flask_security import verify_password, hash_password
+from models import User, db
+from flask import current_app
 
 auth_bp = Blueprint("auth", __name__,  url_prefix="/auth")
 
@@ -25,3 +27,41 @@ def login():
                     "email": user.email,
                     "name": user.name,
                     "token": user.get_auth_token()}), 200
+
+@auth_bp.route("/register", methods=['POST'])
+def register():
+    data = request.get_json()
+    name = data['name']
+    email = data['email']
+    password = data['password']
+    role = data['role']
+
+    active = True
+
+
+    if (not name) or (not email) or (not password):
+        return jsonify({"message":"all three fields are required!"})
+
+    if role == "manager":
+        active = False
+
+    datastore = current_app.datastore
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({"message": "user already exist"}), 400
+
+    datastore.create_user(name=name, email=email, password=hash_password(password), active=active)
+    db.session.commit()
+
+    role=datastore.find_role(role)
+    user = datastore.find_user(email=email)
+    datastore.add_role_to_user(user, role)
+
+    datastore.add_role_to_user(user, role)
+    db.session.commit()
+
+    return jsonify({
+        "id" : user.id,
+        "email": user.email,
+        "name": user.name
+    }), 201
