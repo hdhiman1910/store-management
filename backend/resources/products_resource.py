bc4c385ee85a7ff38fc50ea538306192c290b6da
Harsh Dhiman <hdhiman1910@gmail.com>
2025-12-15 02:39:07 +0530

    refactor ProductResource to use marshaling for response serialization

--- backend/resources/products_resource.py
+++ backend/resources/products_resource.py
@@ -1,23 +1,31 @@
 from flask import request, jsonify
-from flask_restful import Resource
+from flask_restful import Resource, marshal_with, fields
 from services import ProductService
 
+section_fields = {
+    'name': fields.String,
+}
 
-def serialize_product(item):
-    return {
-        "id": item.id,
-        "name": item.name,
-        "price": str(item.price) if item.price is not None else None,
-        "description": item.description,
-        "stock": str(item.stock) if item.stock is not None else None,
-    }
+marshal_fields = {
+    'name': fields.String,
+    'price': fields.Float,
+    'stock': fields.Integer,
+    'expiry': fields.DateTime,
+    'mfd': fields.DateTime,
+    'unit_of_measure': fields.String,
+    'description': fields.String,
+    'section': fields.Nested(section_fields),
+}
+
 
 class ProductResource(Resource):
+    @marshal_with(marshal_fields)
     def get(self, id):
         item = ProductService.get_by_id(id)
-        return jsonify(serialize_product(item))
+        return item
 
 class ProductListResource(Resource):
+    @marshal_with(marshal_fields)
     def get(self):
         items = ProductService.get_all()
-        return jsonify([serialize_product(i) for i in items])
+        return [item for item in items]
